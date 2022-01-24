bc0f631846dca1e8038ee9bc5bde5a364c6dffe7
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-01-24 19:10:34 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-let bill = document.querySelector(".billValue");
-let tip = document.querySelector('input[name="tipPercentage"]:checked');
-let people = document.querySelector(".peopleNumber");
+const bill = document.querySelector(".billValue");
+const tip = document.querySelector('input[name="tipPercentage"]:checked');
+const people = document.querySelector(".peopleNumber");
+const custom = document.querySelector("#custom");
 
-let billValue = bill.value;
 
-console.log(bill);
-console.log(tip);
-console.log(people);
+const reset = document.querySelector(".resetButton");//Para o botão RESET de fato resetar tudo.
+reset.addEventListener("click", function(){
+    bill.value = "";
+    people.value = "";
+    custom.value = "";
 
-console.log(billValue);
+    const inputs = document.querySelectorAll("input"); //Essa função apaga (ou deveria apagar) os inputs selecionados.
+    inputs.forEach(function(input){
+        if (input.hasAttribute("checked") === true) {
+            input.checked = false;
+        }
+    })
+})
+
+
+const percentageSelect = document.querySelectorAll(".percentageOption"); // retorna uma array com todas as DIVS que contêm as porcentagens.
+
+percentageSelect.forEach(function(option){
+
+    option.addEventListener("click",function(){ //cada vez que uma DIV de porcentagem for clicada acontece o seguinte...
+        
+    })
+
+})
