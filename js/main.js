79598edf3dcd9eb05a30d3f709bfe5d0dc295266
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-01-24 14:37:38 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let bill = document.querySelector(".billValue");
+let tip = document.querySelector('input[name="tipPercentage"]:checked');
+let people = document.querySelector(".peopleNumber");
+
+let billValue = bill.value;
+
+console.log(bill);
+console.log(tip);
+console.log(people);
+
+console.log(billValue);
