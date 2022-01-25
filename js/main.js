22cd27a68468aa8a5f1a3a23fad5839e2581f179
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-01-25 12:19:30 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,8 +23,28 @@ const percentageSelect = document.querySelectorAll(".percentageOption"); // reto
 
 percentageSelect.forEach(function(option){
 
-    option.addEventListener("click",function(){ //cada vez que uma DIV de porcentagem for clicada acontece o seguinte...
+    option.addEventListener("click",function(){ //cada vez que uma DIV de porcentagem for clicada muda a cor dela e retorna a cor das outras para a original.
+        percentageSelect.forEach(function(option) {
+            option.style.backgroundColor = "var(--background3)";
+            custom.style.backgroundColor = "var(--background5)";
+        })
+        option.style.backgroundColor = "var(--background4)";
         
     })
 
 })
+
+custom.addEventListener("click", function(){
+    custom.style.backgroundColor = "var(--background4";
+    
+    percentageSelect.forEach(function(option){
+        option.style.backgroundColor = "var(--background3)"
+    })
+})
+
+
+
+
+//Ideia! Foda-se o radio! Coloque as porcentagens direto no JS. Faça com que, se a div dor clicada, ela muda de cor e pronto!
+// Daí o JS, com valores já definidos em uma função, faz as contas.
+// Lembra de fazer as outras divs retornarem à cor original qnd outra for clicada.
